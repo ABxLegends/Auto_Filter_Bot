5b7ac1115f6571f0e103b56d75d55cbb6078552e
ABxLegends <kalapradeesh1986@gmail.com>
2025-08-27 20:54:08 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -107,6 +107,7 @@ async def start(client, message):
                     InlineKeyboardButton('ᴜᴘɢʀᴀᴅᴇ ', callback_data="premium_info"),
                 ],[
                 InlineKeyboardButton('ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url=UPDATE_CHNL_LINK)
+                  ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         current_time = datetime.now(pytz.timezone(TIMEZONE))
         curr_time = current_time.hour        
